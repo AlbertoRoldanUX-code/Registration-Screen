19c576116f05986f9b187bc03595589c2035bb98
Alberto Roldán <a-f-r@hotmail.es>
2021-02-26 21:45:59 +0100

    Set up App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,9 @@
 import React from 'react';
-import './App.css';
-
-
 
 
 
 function App() {
-  return ( <div className = "App">
-    <header className = "App-header" >
-    
-    <p>Edit < code > src / App.js < /code> and save to reload. </p>
-    <a className = "App-link"
-    href = "https://reactjs.org"
-    target = "_blank"
-    rel = "noopener noreferrer" >
-    Learn React <
-    /a> < /
-    header > <
-    footer >
-    <
-    p > Created by Alberto Roldán < /p> <
-    p > Copyright {
-      new Date().getFullYear()
-    } < /p> < /
-    footer > <
-    /div>
+  return ( 
   );
 }
 
