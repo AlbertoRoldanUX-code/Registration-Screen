6d3014d0a520e72afe97fced7a41c04a7bb23e54
Alberto Roldán <a-f-r@hotmail.es>
2021-02-26 22:49:21 +0100

    Show Login screen if user is registered

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,7 @@ var userIsRegistered = false;
 function App() {
   return ( 
   <div className="container">
-  {userIsRegistered?  <h1>Hello</h1> :  <Login />}
+  {userIsRegistered?  <Login /> : <h1>Hello</h1>   }
   </div>
   );
 }
