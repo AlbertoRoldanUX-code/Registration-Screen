6bd10214e6e04e68658ccff38749e1bec938c14e
Alberto Roldán <a-f-r@hotmail.es>
2021-02-26 22:04:07 +0100

    Move input tags from Login component to Input component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 
 function Input(){
-    return();
+    return(<input type="text" placeholder="Username" />);
 }
 
 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,8 +3,7 @@ import React from "react";
 
 function Login(){
     return(<form className="form">
-    <input type="text" placeholder="Username" />
-    <input type="password" placeholder="Password" />
+    
     <button type="submit">Login</button>
   </form>);
 }
