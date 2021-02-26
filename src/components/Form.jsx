1fe975c17da4807681b37d6f0bb6826780311d88
Alberto Roldán <a-f-r@hotmail.es>
2021-02-26 23:01:29 +0100

    Update Form component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 
 function Form(){
-    return ();
+    return (<form className="form">
+    <input type="text" placeholder="Username" />
+    <input type="password" placeholder="Password" />
+    <input type="password" placeholder="Confirm Password" />
+    <button type="submit">Register</button>
+  </form>);
 }
 
 export default Form;
