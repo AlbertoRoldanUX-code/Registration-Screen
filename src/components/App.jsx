1d1f2651cd32ae58c62f04cc81cd6953b84e2f24
Alberto Roldán <a-f-r@hotmail.es>
2021-02-26 21:52:38 +0100

    Create login form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 
 
 function App() {
-  return ( 
+  return ( <div className="container">
+  <h1>Hello</h1>
+  <form className="form">
+    <input type="text" placeholder="Username" />
+    <input type="password" placeholder="Password" />
+    <button type="submit">Login</button>
+  </form>
+</div>
   );
 }
 
